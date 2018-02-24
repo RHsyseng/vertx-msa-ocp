d2fafcaa0b7a08a3e4877851bde2317665d1b014
Babak Mozaffari <bmozaffa@redhat.com>
2018-02-23 19:03:51 -0800

    Added a different implementation of sales/pricing service for A/B testing

---
+++ Sales2/src/main/java/com/redhat/refarch/vertx/lambdaair/sales/service/SalesTicketingService.java
@@ -0,0 +1,135 @@
+package com.redhat.refarch.vertx.lambdaair.sales.service;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import au.com.bytecode.opencsv.CSVReader;
+import com.redhat.refarch.vertx.lambdaair.sales.model.Flight;
+import com.redhat.refarch.vertx.lambdaair.sales.model.FlightSegment;
+import com.redhat.refarch.vertx.lambdaair.sales.model.Itinerary;
+import com.redhat.refarch.vertx.lambdaair.sales.model.Pricing;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.eventbus.Message;
+import io.vertx.core.json.Json;
+
+public class SalesTicketingService extends AbstractVerticle {
+    private static Logger logger = Logger.getLogger(SalesTicketingService.class.getName());
+
+    private static Map<Integer, Pricing> pricingMap = new HashMap<>();
+
+    private static final double extraHopDiscount = 0.9;
+
+    private static final double layoverDiscount = 0.9;
+
+    @Override
+    public void start(Future<Void> startFuture) {
+        vertx.eventBus().consumer(SalesTicketingService.class.getName(), SalesTicketingService::price);
+        loadPricing().setHandler(startFuture.completer());
+    }
+
+    private static Future<Void> loadPricing() {
+        Future<Void> future = Future.future();
+        try {
+            if (pricingMap.isEmpty()) {
+                InputStream inputStream = SalesTicketingService.class.getResourceAsStream("/pricing.csv");
+                CSVReader reader = new CSVReader(new InputStreamReader(inputStream), '\t');
+                String[] nextLine;
+                while ((nextLine = reader.readNext()) != null) {
+                    Pricing pricing = new Pricing();
+                    pricing.flightNumber = Integer.parseInt(nextLine[0]);
+                    pricing.basePrice = Integer.parseInt(nextLine[1]);
+                    pricing.availability = new int[nextLine.length - 2];
+                    for (int index = 2; index < nextLine.length; index++) {
+                        pricing.availability[index - 2] = Integer.parseInt(nextLine[index]);
+                    }
+                    pricingMap.put(pricing.flightNumber, pricing);
+                }
+                logger.info("SalesTicketingService successfully initialized");
+            }
+            future.complete();
+        } catch (IOException e) {
+            future.fail(e);
+        }
+        return future;
+    }
+
+    private static void price(Message<String> message) {
+        Flight flight = Json.decodeValue(message.body(), Flight.class);
+        int dateOffset = getDateOffset(flight.getSegments()[0].getDepartureTime());
+        int price = 0;
+        Instant arrival = null;
+        for (FlightSegment flightSegment : flight.getSegments()) {
+            Pricing pricing = pricingMap.get(flightSegment.getFlightNumber());
+            int segmentPrice = pricing.basePrice;
+            segmentPrice *= getAvailabilityDiscount(dateOffset, pricing.availability[dateOffset - 1]);
+            if (price == 0) {
+                price = segmentPrice;
+            } else {
+                int hopPrice = segmentPrice;
+                int layover = (int) Duration.between(arrival, flightSegment.getDepartureTime()).toHours() - 1;
+                if (layover > 0) {
+                    //Apply a discount for every extra hour of layover:
+                    hopPrice *= layover * layoverDiscount;
+                }
+                //Add to total price so far:
+                price += hopPrice;
+                //Apply another discount on top of total for extra hop:
+                price *= extraHopDiscount;
+            }
+            arrival = flightSegment.getArrivalTime();
+        }
+        Itinerary itinerary = new Itinerary(flight);
+        logger.fine("Priced ticket at " + price);
+        itinerary.setPrice(price);
+        message.reply(Json.encode(itinerary));
+    }
+
+    private static double getAvailabilityDiscount(int dateOffset, int availability) {
+        if (dateOffset <= 3) {
+            if (availability > 60) {
+                return 0.7;
+            } else if (availability > 50) {
+                return 0.8;
+            } else {
+                return 1;
+            }
+        } else if (dateOffset <= 7) {
+            if (availability > 70) {
+                return 0.75;
+            } else {
+                return 1;
+            }
+        } else if (dateOffset <= 21) {
+            if (availability > 90) {
+                return 0.6;
+            } else if (availability > 80) {
+                return 0.8;
+            } else {
+                return 1;
+            }
+        } else {
+            if (availability > 90) {
+                return 0.9;
+            } else {
+                return 1;
+            }
+        }
+    }
+
+    private static int getDateOffset(Instant instant) {
+        int dateOffset = (int) Duration.between(Instant.now(), instant).toDays();
+        if (dateOffset < 1) {
+            dateOffset = 1;
+        } else if (dateOffset > 60) {
+            dateOffset = 60;
+        }
+        return dateOffset;
+    }
+}
---
+++ Sales2/src/main/java/com/redhat/refarch/vertx/lambdaair/sales/service/Vertical.java
@@ -0,0 +1,123 @@
+package com.redhat.refarch.vertx.lambdaair.sales.service;
+
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.uber.jaeger.Configuration;
+import com.uber.jaeger.Span;
+import io.opentracing.contrib.vertx.ext.web.TracingHandler;
+import io.vertx.config.ConfigRetriever;
+import io.vertx.config.ConfigRetrieverOptions;
+import io.vertx.config.ConfigStoreOptions;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Future;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.eventbus.Message;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+
+public class Vertical extends AbstractVerticle {
+    private static Logger logger = Logger.getLogger(Vertical.class.getName());
+
+    @Override
+    public void start(Future<Void> startFuture) {
+        Json.mapper.registerModule(new JavaTimeModule());
+        ConfigStoreOptions store = new ConfigStoreOptions();
+        store.setType("file").setFormat("yaml").setConfig(new JsonObject().put("path", "app-config.yml"));
+        ConfigRetriever retriever = ConfigRetriever.create(vertx, new ConfigRetrieverOptions().addStore(store));
+        retriever.getConfig(result -> startWithConfig(startFuture, result));
+    }
+
+    private void startWithConfig(Future<Void> startFuture, AsyncResult<JsonObject> configResult) {
+        if (configResult.failed()) {
+            throw new IllegalStateException(configResult.cause());
+        }
+        mergeIn(null, configResult.result().getMap());
+
+        DeploymentOptions deploymentOptions = new DeploymentOptions().setWorker(true);
+        vertx.deployVerticle(new SalesTicketingService(), deploymentOptions, deployResult -> {
+            if (deployResult.succeeded()) {
+                Router router = Router.router(vertx);
+                router.get("/health").handler(routingContext -> routingContext.response().end("OK"));
+                router.post("/price").handler(Vertical.this::price);
+                setupTracing(router);
+                HttpServer httpServer = vertx.createHttpServer();
+                httpServer.requestHandler(router::accept);
+                int port = config().getInteger("http.port", 8080);
+                httpServer.listen(port, result -> {
+                    if (result.succeeded()) {
+                        startFuture.succeeded();
+                        logger.info("Running http server on port " + result.result().actualPort());
+                    } else {
+                        startFuture.fail(result.cause());
+                    }
+                });
+            } else {
+                startFuture.fail(deployResult.cause());
+            }
+        });
+
+    }
+
+    private void mergeIn(String prefix, Map<String, Object> map) {
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            String key;
+            if (prefix == null) {
+                key = entry.getKey();
+            } else {
+                key = prefix + "." + entry.getKey();
+            }
+            Object value = entry.getValue();
+            if (value instanceof Map) {
+                //noinspection unchecked
+                mergeIn(key, (Map<String, Object>) value);
+            } else {
+                config().put(key, value);
+            }
+        }
+    }
+
+    private void setupTracing(Router router) {
+        Configuration.SamplerConfiguration samplerConfiguration = new Configuration.SamplerConfiguration(config().getString("JAEGER_SAMPLER_TYPE"), config().getDouble("JAEGER_SAMPLER_PARAM"), config().getString("JAEGER_SAMPLER_MANAGER_HOST_PORT"));
+        Configuration.ReporterConfiguration reporterConfiguration = new Configuration.ReporterConfiguration(config().getBoolean("JAEGER_REPORTER_LOG_SPANS"), config().getString("JAEGER_AGENT_HOST"), config().getInteger("JAEGER_AGENT_PORT"), config().getInteger("JAEGER_REPORTER_FLUSH_INTERVAL"), config().getInteger("JAEGER_REPORTER_MAX_QUEUE_SIZE"));
+        Configuration configuration = new Configuration(config().getString("JAEGER_SERVICE_NAME"), samplerConfiguration, reporterConfiguration);
+        TracingHandler tracingHandler = new TracingHandler(configuration.getTracer());
+        router.route().order(-1).handler(tracingHandler).failureHandler(tracingHandler);
+    }
+
+    private void price(RoutingContext routingContext) {
+        getActiveSpan(routingContext).setTag("Operation", "Determine Price for a Flight");
+        routingContext.request().bodyHandler(buffer -> handleIt(routingContext, buffer));
+    }
+
+    private void handleIt(RoutingContext routingContext, Buffer buffer) {
+        vertx.eventBus().<String>send(SalesTicketingService.class.getName(), buffer.toString(), asyncResult -> respondPrice(routingContext, asyncResult));
+    }
+
+
+    private void respondPrice(RoutingContext routingContext, AsyncResult<Message<String>> reply) {
+        HttpServerResponse response = routingContext.response();
+        if (reply.succeeded()) {
+            response.putHeader("Content-Type", "application/json; charset=utf-8");
+            response.end(reply.result().body());
+        } else {
+            Throwable exception = reply.cause();
+            logger.log(Level.SEVERE, "Failed to price ticket", exception);
+            response.setStatusCode(500);
+            response.setStatusMessage(exception.getMessage());
+            response.end();
+        }
+    }
+
+    private Span getActiveSpan(RoutingContext routingContext) {
+        return routingContext.get(TracingHandler.CURRENT_SPAN);
+    }
+}
